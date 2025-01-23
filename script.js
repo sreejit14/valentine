7bc47cc6ac5965365ab35e257a087c5b3b678f0b
sreejit chakraborty <143126776+sreejit14@users.noreply.github.com>
2025-01-23 23:33:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function moveRedButton() {
+    const redButton = document.querySelector('.no');
+    const body = document.body;
+    const maxX = body.clientWidth - redButton.offsetWidth;
+    const maxY = body.clientHeight - redButton.offsetHeight;
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+    redButton.style.left = randomX + 'px';
+    redButton.style.top = randomY + 'px';
+}
+
+document.querySelector('.no').addEventListener('mouseover', moveRedButton);
+
+document.querySelector('.yes').addEventListener('click', function() {
+    document.querySelector('h1').textContent = 'Yess I know you love me';
+    document.querySelector('.center-gif').innerHTML = '<img src="My Love Flower GIF by cintascotch.gif" alt="Flower GIF">';
+    document.querySelector('.button-container').style.display = 'none';
+    document.querySelector('.changing-text').textContent = 'I love you too my cutiee';
+    document.body.style.backgroundColor = 'purple'; 
+});
+
+const texts = [
+    "erom korbi tui",
+    "dhorte parlonaa!",
+    "ebabaa!",
+    "yes bole de na maaa",
+    "ebar kende dbo"
+];
+
+document.querySelector('.no').addEventListener('mouseover', function() {
+    const randomIndex = Math.floor(Math.random() * texts.length);
+    document.querySelector('.changing-text').textContent = texts[randomIndex];
+});
